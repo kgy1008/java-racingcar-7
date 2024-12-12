a2ec5650d438d27a6011f9fb67bd52c82f34b87d
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 20:01:16 +0900

    feat: 자동차 이름 검증 조건 추가

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,12 +1,23 @@
 package racingcar.domain;
 
+import racingcar.common.ErrorMessage;
+
 public class Car {
+
+    private static final int MAX_LENGTH = 5;
 
     private final String name;
     private final int position;
 
     public Car(final String name, final int position) {
+        validateName(name);
         this.name = name;
         this.position = position;
+    }
+
+    private void validateName(final String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
+        }
     }
 }
