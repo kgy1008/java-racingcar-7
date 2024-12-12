c4ed1c2c9dd30e9406bac8ca3dfc7a492751cb3b
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 19:52:13 +0900

    feat: 입력 안내 메세지 출력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,12 +6,14 @@ import racingcar.common.ErrorMessage;
 public class InputView {
 
     public String inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         validate(input);
         return input;
     }
 
     public int inputTryCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
         String input = Console.readLine();
         return convertToInt(input);
     }
