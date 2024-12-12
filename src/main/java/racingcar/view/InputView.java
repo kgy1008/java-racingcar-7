ab39246d67b78425dfcf97430858c895838f1a06
kgy1008 <gayeon1008@g.skku.edu>
2024-12-12 19:49:01 +0900

    feat: 사용자로부터 값 입력받기

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,12 +1,32 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.common.ErrorMessage;
 
 public class InputView {
 
+    public String inputCarNames() {
+        String input = Console.readLine();
+        validate(input);
+        return input;
+    }
+
+    public int inputTryCount() {
+        String input = Console.readLine();
+        return convertToInt(input);
+    }
+
     private void validate(final String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
+        }
+    }
+
+    private int convertToInt(final String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT_TYPE.getMessage());
         }
     }
 }
